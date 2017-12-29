be38ced8219e198d02b7d4141721253f38ed3d2b
AlexeiP <alexei.palii@gmail.com>
2017-12-30 01:29:28 +0300

    php lab 3 & 4

---
+++ APalii/php_mysql_3_4/php2/test2.php
@@ -0,0 +1,117 @@
+
+
+<?php
+
+
+phpinfo();
+
+massive
+$arrayName = array("key" => 1000);
+echo $arrayName['key'];
+
+####
+$arr['a' => 100];
+var_dump($arr['a']);
+
+####
+
+$arr[100 , 200, 300 , 6 => 500, 5045];
+var_dump($arr['5']);  /// null;
+
+
+Iteration!!!
+
+$_POST
+$_GET
+$_SERVER
+
+foreach($arr as $key => $value){
+	echo "key" . $key . "value" . $value;
+}
+
+echo "<pre>";
+print_r($_GET);
+echo "</pre>";
+
+###############
+
+<?php  
+$str  = 'title';
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+	<h1><?php echo $str; ?></h1>
+
+</body>
+</html>
+
+
+
+
+
+
+$x;
+var_dump($x);
+undefined --- in php will be NULL;
+
+
+
+
+const max = 5; -- define constant
+define(max,100);
+
+max = 7;
+
+$x = 100;
+$str1 = "efwf";
+$str2 = 'ddewqdw';
+
+echo $str1;
+echo $str2;
+
+
+echo "stag1";
+
+?>
+
+<?phph
+
+echo "stage2";
+
+if(!0)
+
+echo "if";
+
+
+
+if(q;dqwd;qwdwq){
+	dqwdwqd
+}
+
+$i = 0;
+$i++;
+echo "expr 1 "$i++;
+echo "expr 2 "++$i;
+
+for ($i=0; $i>0 ; ++$i++)
+
+$x = (true)? ;   ; 
+
+var_dump(stage2); -- for debuging
+
+function addFile(){
+	return 0;
+}
+
+$str = addFile();
+
+var_dump($srt);
+
+?>
+
