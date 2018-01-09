0a4353af982f9d85e260eef58a5cb2c45eae006b
Oleg <fomichevolegv@gmail.com>
2018-01-09 17:44:54 +0300

    1

---
+++ Fomichev/php/lab2/servicer.php
@@ -0,0 +1,14 @@
+
+<?php
+$content = file_get_contents('tmpl/'.date('d.m.o').'.txt'); //находим файл по текущей дате и сохраняем его в переменную.
+$slice = explode(";", $content); // разбиваем строку по разделтелю
+$event = $slice[1];
+$subscribe = $slice[2];
+$obj = array('date' => date('d.m.o'), 'event' => $event, 'subscribe' => $subscribe);
+echo json_encode($obj);
+?>
+
+
+
+
+    
