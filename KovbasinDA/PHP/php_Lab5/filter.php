feb5661d91f1aabb2500c2bf366ba0775086a312
diakov312 <dkovbasin@gmail.com>
2018-01-10 14:07:29 +0300

    Added php lab5. Login: admin Password: 123.

---
+++ KovbasinDA/PHP/php_Lab5/filter.php
@@ -0,0 +1,28 @@
+<?php
+?>
+
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>php_Lab1</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <script src="js/jquery-3.2.1.min.js"></script>
+    <script src="js/dateFormat.js"></script>
+    <script src="js/mustache.js"></script>
+
+
+
+    <div id="headerFilter">
+        <label for="">Вывести события с: </label><input type="date" id="inpFirstDateFilter" name="firstDateFilter">
+        <label for=""></label> по: <input type="date" id="inpSecondDateFilter" name="secondDateFilter">
+        <button type="button" id="searchButton">Поиск</button>
+    </div>
+    <script src="js/filter.js"></script>
+
+    <div id="eventRibbon"></div>
+
+
+</body>
+</html>
