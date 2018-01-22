5784b366e613cfc1b58a85dee9f643f1e33bb4f4
Oleg <fomichevolegv@gmail.com>
2018-01-22 23:52:19 +0300

    n

---
+++ Fomichev/php/lab5/admin/validation.php
@@ -0,0 +1,16 @@
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <title>php_Lab1</title>
+        <link rel="stylesheet" href="../css/style.css">
+    </head>
+    <body>
+        <div id="day-calender">
+            <form action="index.php" method="post">
+                <div><label for="inpLogin">Login:</label><input id="inpLogin" type="text" name="nLogin"></div>
+                <div><label for="inpPassword">Password:</label><input id="inpPassword" type="password" name="nPassword" ></div>
+                <div><button type="submit" style="width: 300px">Send</button></div>
+            </form>
+        </div>
+    </body>
+</html>
